5dc8d56f9a1bc7375b70cbad9a8e5c2279abe93d
methmaaravinda <methmaaravinda@gmail.com>
2024-03-11 13:41:44 +0530

    build bug fix

--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -1,5 +1,5 @@
 export default function ChatMessage(props) {
-    const { text, uid, photoURL } = props.message;
+    const { text, uid } = props.message;
   
     const messageClass = uid === 1 ? 'sent' : 'received';
     const imgPath = uid ===1 ? "/profileImg/1.jfif": "/profileImg/2.png"
