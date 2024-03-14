d51cabee1b238e9225cc97e119cfa758661bb0ba
methmaaravinda <methmaaravinda@gmail.com>
2024-03-14 12:28:14 +0530

    update

--- src/App.js
+++ src/App.js
@@ -9,21 +9,36 @@ import ChatList from './components/ChatList';
 // import { getChatsByClient } from './db/quaries';
 // import { addUser } from './db/auth';
 // import { AuthContext } from './context/Auth';
+import { Navbar, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import DBTest from './components/DBTest';
 import ShowDataDb from './components/ShowDataDb';
 
 function App() {
-  // const {auth, setAuth} = useContext(AuthContext);
-  return (<>
-  <Routes>
-    <Route path='/' element = {<SignUpForm/>}/>
-    <Route path='/chat' element = {<ChatRoom/>}/>
-    <Route path='/chatList' element = {<ChatList/>}/>
-    <Route path='/dbTest' element = {<DBTest/>}/>
-    <Route path='/showData' element = {<ShowDataDb/>}/>
-  </Routes> 
-  </>);
-}
+  return (
+    <>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand as={Link} to="/">Your App Name</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/chat">Chat</Nav.Link>
+            <Nav.Link as={Link} to="/chatList">Chat List</Nav.Link>
+            <Nav.Link as={Link} to="/dbTest">DB Test</Nav.Link>
+            <Nav.Link as={Link} to="/showData">Show Data</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
 
+      <Routes>
+        <Route path='/' element={<SignUpForm />} />
+        <Route path='/chat' element={<ChatRoom />} />
+        <Route path='/chatList' element={<ChatList />} />
+        <Route path='/dbTest' element={<DBTest />} />
+        <Route path='/showData' element={<ShowDataDb />} />
+      </Routes>
+    </>
+  );
+}
 
 export default App;
